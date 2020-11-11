9634b77afcf9b67a874f5e8484f17c147d4b59ea
Le Tung <kyle.le@deliveree.com>
2020-11-12 00:01:19 +0700

    Add Secure API for Movies

--- app/models/constants.rb
+++ app/models/constants.rb
@@ -2,5 +2,8 @@ module Constants
     class PublicMoviesController
       ResponseFieldsAllow = ["id", "title", "description", "user_id"]
     end
-  
+
+    class SecureMoviesController
+      ResponseFieldsAllow = ["id", "title", "description", "user_id", "upvote", "downvote"]
+    end
 end
--- app/services/authentication_service.rb
+++ app/services/authentication_service.rb
@@ -1,2 +1,24 @@
 class AuthenticationService
+  def initialize(token)
+    @token = token
+    @user = nil
+  end
+
+  def process
+    authenticate
+  end
+
+  def valid?
+    @user.present?
+  end
+
+  def current_user
+    @user
+  end
+
+  private
+
+  def authenticate
+    @user = User.find_by(authentication_token: @token)
+  end
 end
